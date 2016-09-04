b3d1798915a5da7721d0cb8bc8839332f8283123
DanieLITO <jimyyarlequeesqueche@users.noreply.github.com>
2016-09-04 04:16:16 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,424 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.L_Can = New System.Windows.Forms.Label()
+        Me.L_Art = New System.Windows.Forms.Label()
+        Me.L_Añ = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.la = New System.Windows.Forms.Label()
+        Me.ae = New System.Windows.Forms.Label()
+        Me.labell = New System.Windows.Forms.Label()
+        Me.labela = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.L_Tot = New System.Windows.Forms.Label()
+        Me.L_impSasha = New System.Windows.Forms.Label()
+        Me.L_preSasha = New System.Windows.Forms.Label()
+        Me.L_preQeen = New System.Windows.Forms.Label()
+        Me.L_ImpQueen = New System.Windows.Forms.Label()
+        Me.L_preBeeGees = New System.Windows.Forms.Label()
+        Me.L_ImpBeeGees = New System.Windows.Forms.Label()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.L_Añ)
+        Me.Panel1.Controls.Add(Me.L_Art)
+        Me.Panel1.Controls.Add(Me.L_Can)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(20, 10)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(299, 116)
+        Me.Panel1.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(14, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(109, 97)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'L_Can
+        '
+        Me.L_Can.AutoSize = True
+        Me.L_Can.Location = New System.Drawing.Point(153, 12)
+        Me.L_Can.Name = "L_Can"
+        Me.L_Can.Size = New System.Drawing.Size(46, 13)
+        Me.L_Can.TabIndex = 1
+        Me.L_Can.Text = "Cancion"
+        '
+        'L_Art
+        '
+        Me.L_Art.AutoSize = True
+        Me.L_Art.Location = New System.Drawing.Point(150, 43)
+        Me.L_Art.Name = "L_Art"
+        Me.L_Art.Size = New System.Drawing.Size(36, 13)
+        Me.L_Art.TabIndex = 2
+        Me.L_Art.Text = "Artista"
+        '
+        'L_Añ
+        '
+        Me.L_Añ.AutoSize = True
+        Me.L_Añ.Location = New System.Drawing.Point(153, 83)
+        Me.L_Añ.Name = "L_Añ"
+        Me.L_Añ.Size = New System.Drawing.Size(26, 13)
+        Me.L_Añ.TabIndex = 3
+        Me.L_Añ.Text = "Año"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.L_impSasha)
+        Me.Panel2.Controls.Add(Me.L_preSasha)
+        Me.Panel2.Controls.Add(Me.Button4)
+        Me.Panel2.Controls.Add(Me.ae)
+        Me.Panel2.Controls.Add(Me.la)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Location = New System.Drawing.Point(20, 132)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(401, 105)
+        Me.Panel2.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Controls.Add(Me.L_ImpQueen)
+        Me.Panel3.Controls.Add(Me.L_preQeen)
+        Me.Panel3.Controls.Add(Me.Button5)
+        Me.Panel3.Controls.Add(Me.labell)
+        Me.Panel3.Controls.Add(Me.labela)
+        Me.Panel3.Controls.Add(Me.Button2)
+        Me.Panel3.Location = New System.Drawing.Point(20, 243)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(401, 99)
+        Me.Panel3.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(309, 26)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(74, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Comprar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'la
+        '
+        Me.la.AutoSize = True
+        Me.la.Location = New System.Drawing.Point(11, 15)
+        Me.la.Name = "la"
+        Me.la.Size = New System.Drawing.Size(37, 13)
+        Me.la.TabIndex = 4
+        Me.la.Text = "Precio"
+        '
+        'ae
+        '
+        Me.ae.AutoSize = True
+        Me.ae.Location = New System.Drawing.Point(10, 58)
+        Me.ae.Name = "ae"
+        Me.ae.Size = New System.Drawing.Size(42, 13)
+        Me.ae.TabIndex = 5
+        Me.ae.Text = "Importe"
+        '
+        'labell
+        '
+        Me.labell.AutoSize = True
+        Me.labell.Location = New System.Drawing.Point(13, 64)
+        Me.labell.Name = "labell"
+        Me.labell.Size = New System.Drawing.Size(42, 13)
+        Me.labell.TabIndex = 8
+        Me.labell.Text = "Importe"
+        '
+        'labela
+        '
+        Me.labela.AutoSize = True
+        Me.labela.Location = New System.Drawing.Point(13, 13)
+        Me.labela.Name = "labela"
+        Me.labela.Size = New System.Drawing.Size(37, 13)
+        Me.labela.TabIndex = 7
+        Me.labela.Text = "Precio"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(308, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(74, 32)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Comprar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Panel4
+        '
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel4.Controls.Add(Me.L_ImpBeeGees)
+        Me.Panel4.Controls.Add(Me.L_preBeeGees)
+        Me.Panel4.Controls.Add(Me.Button6)
+        Me.Panel4.Controls.Add(Me.Label8)
+        Me.Panel4.Controls.Add(Me.Label9)
+        Me.Panel4.Controls.Add(Me.Button3)
+        Me.Panel4.Location = New System.Drawing.Point(20, 348)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(401, 104)
+        Me.Panel4.TabIndex = 3
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(11, 56)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(42, 13)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Importe"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(11, 13)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(37, 13)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "Precio"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(308, 23)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(74, 32)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Comprar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(220, 31)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(71, 22)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Informacion"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(219, 23)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(71, 22)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Informacion"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(220, 28)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(71, 22)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "Informacion"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Panel5
+        '
+        Me.Panel5.Controls.Add(Me.L_Tot)
+        Me.Panel5.Controls.Add(Me.Label1)
+        Me.Panel5.Location = New System.Drawing.Point(20, 458)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(401, 47)
+        Me.Panel5.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "PRESIO"
+        '
+        'L_Tot
+        '
+        Me.L_Tot.BackColor = System.Drawing.Color.White
+        Me.L_Tot.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.L_Tot.Location = New System.Drawing.Point(302, 12)
+        Me.L_Tot.Name = "L_Tot"
+        Me.L_Tot.Size = New System.Drawing.Size(81, 28)
+        Me.L_Tot.TabIndex = 10
+        '
+        'L_impSasha
+        '
+        Me.L_impSasha.BackColor = System.Drawing.Color.White
+        Me.L_impSasha.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.L_impSasha.Location = New System.Drawing.Point(67, 57)
+        Me.L_impSasha.Name = "L_impSasha"
+        Me.L_impSasha.Size = New System.Drawing.Size(30, 30)
+        Me.L_impSasha.TabIndex = 11
+        '
+        'L_preSasha
+        '
+        Me.L_preSasha.BackColor = System.Drawing.Color.White
+        Me.L_preSasha.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.L_preSasha.Location = New System.Drawing.Point(67, 14)
+        Me.L_preSasha.Name = "L_preSasha"
+        Me.L_preSasha.Size = New System.Drawing.Size(76, 30)
+        Me.L_preSasha.TabIndex = 12
+        '
+        'L_preQeen
+        '
+        Me.L_preQeen.BackColor = System.Drawing.Color.White
+        Me.L_preQeen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.L_preQeen.Location = New System.Drawing.Point(67, 12)
+        Me.L_preQeen.Name = "L_preQeen"
+        Me.L_preQeen.Size = New System.Drawing.Size(76, 30)
+        Me.L_preQeen.TabIndex = 13
+        '
+        'L_ImpQueen
+        '
+        Me.L_ImpQueen.BackColor = System.Drawing.Color.White
+        Me.L_ImpQueen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.L_ImpQueen.Location = New System.Drawing.Point(67, 63)
+        Me.L_ImpQueen.Name = "L_ImpQueen"
+        Me.L_ImpQueen.Size = New System.Drawing.Size(76, 30)
+        Me.L_ImpQueen.TabIndex = 14
+        '
+        'L_preBeeGees
+        '
+        Me.L_preBeeGees.BackColor = System.Drawing.Color.White
+        Me.L_preBeeGees.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.L_preBeeGees.Location = New System.Drawing.Point(67, 12)
+        Me.L_preBeeGees.Name = "L_preBeeGees"
+        Me.L_preBeeGees.Size = New System.Drawing.Size(76, 30)
+        Me.L_preBeeGees.TabIndex = 13
+        '
+        'L_ImpBeeGees
+        '
+        Me.L_ImpBeeGees.BackColor = System.Drawing.Color.White
+        Me.L_ImpBeeGees.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.L_ImpBeeGees.Location = New System.Drawing.Point(67, 55)
+        Me.L_ImpBeeGees.Name = "L_ImpBeeGees"
+        Me.L_ImpBeeGees.Size = New System.Drawing.Size(76, 30)
+        Me.L_ImpBeeGees.TabIndex = 14
+        '
+        'Button7
+        '
+        Me.Button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button7.Location = New System.Drawing.Point(366, 18)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(38, 39)
+        Me.Button7.TabIndex = 11
+        Me.Button7.Text = "X"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(108, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 29)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "%"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(449, 517)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents L_Añ As Label
+    Friend WithEvents L_Art As Label
+    Friend WithEvents L_Can As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents ae As Label
+    Friend WithEvents la As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents labell As Label
+    Friend WithEvents labela As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents L_Tot As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents L_impSasha As Label
+    Friend WithEvents L_preSasha As Label
+    Friend WithEvents L_ImpQueen As Label
+    Friend WithEvents L_preQeen As Label
+    Friend WithEvents L_ImpBeeGees As Label
+    Friend WithEvents L_preBeeGees As Label
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'Informacion del Artista
+        Dim precio As Double = 15.5
+        Dim Impor As Double = 2%
+        Dim Can As String = "Remix Sasha Grey"
+        Dim Art As String = "Sasha Grey"
+        Dim Año As String = "2016"
+        'llamar a la imagen 
+
+        'PictureBox1.Image = Image.FromFile("" + CStr("Sashaimag") + ".jpg")
+        ' LLamar a los laves
+        L_Can.Text = Can.ToString() : L_Art.Text = Art.ToString() : L_Añ.Text = Año.ToString()
+        L_preSasha.Text = precio.ToString : L_impSasha.Text = Impor.ToString
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'codgo de la compra para el primer artista
+        Dim precio As Double = 15.5
+        Dim imp As Double = precio * 0.2
+        Dim total As Double = precio + imp
+        'hbsand
+
+        L_Tot.Text = total.ToString
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Informacion del Artista
+        Dim Can As String = "Bohemian Rhapsody"
+        Dim Art As String = "The Queen"
+        Dim Año As String = "1980"
+        ' LLamar a los laves
+        L_Can.Text = Can.ToString() : L_Art.Text = Art.ToString() : L_Añ.Text = Año.ToString()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        'Informacion del Artista
+        Dim Can As String = "Night Fever"
+        Dim Art As String = "Bee Gees"
+        Dim Año As String = "1977"
+        ' LLamar a los laves
+        L_Can.Text = Can.ToString() : L_Art.Text = Art.ToString() : L_Añ.Text = Año.ToString()
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Me.Close()
+    End Sub
+End Class
